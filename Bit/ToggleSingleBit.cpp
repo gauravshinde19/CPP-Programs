6400aad13596ddc476bac7e34ff4ebaa835b8947
Gaurav Mahadev Shinde <80934597+gauravshinde19@users.noreply.github.com>
2024-08-05 13:29:14 +0530

    Create ToggleSingleBit.cpp

---
+++ Bit/ToggleSingleBit.cpp
@@ -0,0 +1,29 @@
+// Program to accept number from user and toggle 7th bit of the number. Return modified number.
+
+#include <iostream>
+using namespace std;
+
+int ToggleBit(int iNo)
+{
+    unsigned int iMask = 0X00000040;
+    unsigned int iResult = 0;
+
+    iResult = iNo ^ iMask;
+
+    return iResult;
+}
+
+int main()
+{
+    unsigned int iValue = 0;
+    unsigned int iRet = 0;
+
+    cout << "Enter the number :" << endl;
+    cin >> iValue;
+
+    iRet = ToggleBit(iValue);
+
+    cout << "Updated number : " << endl << iRet;
+
+    return 0;
+}
